2da5feccaf31aabe6bb0e70702df8fe606b0f958
Ricky Hu <ricky9667@gmail.com>
2020-08-28 18:28:35 +0800

    numbers can change when hit by other numbers

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -17,6 +17,13 @@ class GamePanel extends StatefulWidget {
 }
 
 class _GamePanelState extends State<GamePanel> {
+  List<int> numbers = List.filled(4, 1);
+  int chosenIndex = -1;
+
+  void changeNumberValue({int index, int value}) {
+    numbers[index] = value;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,17 +35,37 @@ class _GamePanelState extends State<GamePanel> {
             children: [
               PlayerButton(
                 buttonPressed: () {
+                  setState(() {
+                    if (chosenIndex == -1) {
+                      chosenIndex = 0;
+                    } else if (chosenIndex == 0) {
+                      chosenIndex = -1;
+                    } else {
+                      changeNumberValue(index: 0, value: numbers[0]+numbers[chosenIndex]);
+                      chosenIndex = -1;
+                    }
+                  });
                   print('Player 1: right hand clicked');
                 },
-                buttonText: '1',
-                backgroundColor: Colors.white,
+                buttonText: numbers[0].toString(),
+                backgroundColor: (chosenIndex == 0 ? Colors.blue : Colors.white),
               ),
               PlayerButton(
                 buttonPressed: () {
+                  setState(() {
+                    if (chosenIndex == -1) {
+                      chosenIndex = 1;
+                    } else if (chosenIndex == 1) {
+                      chosenIndex = -1;
+                    } else {
+                      changeNumberValue(index: 1, value: numbers[1]+numbers[chosenIndex]);
+                      chosenIndex = -1;
+                    }
+                  });
                   print('Player 1: left hand clicked');
                 },
-                buttonText: '1',
-                backgroundColor: Colors.white,
+                buttonText: numbers[1].toString(),
+                backgroundColor: (chosenIndex == 1 ? Colors.blue : Colors.white),
               ),
             ],
           ),
@@ -47,17 +74,37 @@ class _GamePanelState extends State<GamePanel> {
             children: [
               PlayerButton(
                 buttonPressed: () {
+                  setState(() {
+                    if (chosenIndex == -1) {
+                      chosenIndex = 2;
+                    } else if (chosenIndex == 2) {
+                      chosenIndex = -1;
+                    } else {
+                      changeNumberValue(index: 2, value: numbers[2]+numbers[chosenIndex]);
+                      chosenIndex = -1;
+                    }
+                  });
                   print('Player 2: left hand clicked');
                 },
-                buttonText: '1',
-                backgroundColor: Colors.white,
+                buttonText: numbers[2].toString(),
+                backgroundColor: (chosenIndex == 2 ? Colors.red : Colors.white),
               ),
               PlayerButton(
                 buttonPressed: () {
+                  setState(() {
+                    if (chosenIndex == -1) {
+                      chosenIndex = 3;
+                    } else if (chosenIndex == 3) {
+                      chosenIndex = -1;
+                    } else {
+                      changeNumberValue(index: 3, value: numbers[3]+numbers[chosenIndex]);
+                      chosenIndex = -1;
+                    }
+                  });
                   print('Player 2: right hand clicked');
                 },
-                buttonText: '1',
-                backgroundColor: Colors.white,
+                buttonText: numbers[3].toString(),
+                backgroundColor: (chosenIndex == 3 ? Colors.red : Colors.white),
               ),
             ],
           )
@@ -66,3 +113,5 @@ class _GamePanelState extends State<GamePanel> {
     );
   }
 }
+
+
